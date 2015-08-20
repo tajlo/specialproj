87c1b7a014de176c3e3ccb72bc985e3840bdcc9d
Tajaa Long <tajaalong@Tajaas-MacBook-Pro.local>
2015-08-20 13:37:56 -0400

    number 1 added

--- public/reference.js
+++ public/reference.js
@@ -124,7 +124,7 @@ $scope.address = {
 }
 
 $scope.people = {
-   number: [2, 3 , 4, 5 ,6 , 7, 8 , 9, 10, 11 , 12]
+   number: [1, 2, 3 , 4, 5 ,6 , 7, 8 , 9, 10, 11 , 12]
 }
 
 $scope.showing = false
